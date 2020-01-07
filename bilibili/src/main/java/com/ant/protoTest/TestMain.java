8270becf4f8b3bbc0b6f75d7e116f94e68b71d01
xuchuanliang <xuchuanliang361@163.com>
2020-01-07 13:10:26 +0800

    protobuf

---
+++ bilibili/src/main/java/com/ant/protoTest/TestMain.java
@@ -0,0 +1,17 @@
+package com.ant.protoTest;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class TestMain {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        Person.Info info = Person.Info.newBuilder().setId("徐传良").setName("哈哈哈").build();
+        byte[] array = info.toByteArray();
+        Person.Info info1 = Person.Info.parseFrom(array);
+        System.out.println(info1.getId());
+        System.out.println(info1.getName());
+        for(int i = 0;i<array.length;i++){
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+    }
+}
